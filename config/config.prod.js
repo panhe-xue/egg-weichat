c080cb70defa1e0a42d8b5bce59921091421c8f6
panhe.com <1158817942@qq.com>
2020-02-23 10:55:12 +0800

    change config

--- config/config.prod.js
+++ config/config.prod.js
@@ -24,20 +24,28 @@ module.exports = () => {
     consoleLevel: 'DEBUG',
     disableConsoleAfterReady: false,
     level: 'INFO', // 避免记录数据库执行语句
-    dir: path.join(__dirname, '../logs/pro/'),
+    dir: path.join(__dirname, '../../logs/pro/'),
   };
   // 性能分析配置
   config.alinode = {
     server: 'wss://agentserver.node.aliyun.com:8080',
-    appid: '81214',
-    secret: '899b8fceed93c8efda670a254c5211526e596cfd',
+    appid: '83784',
+    secret: '771de395d0ef91054dddf46eadf5eef079dfbd03',
     // logdir: path.join(__dirname, '../logs/test/'),
     // error_log: [],
     // agentidMode:'IP' '可选，如果设置，则在实例ID中添加部分IP信息，用于多个实例 hostname 相同的场景（以容器为主）'
   };
   config.uploads = {
     uploadAddr: path.join(__dirname, '../../uploads/'),
-    dataBaseAddr: 'https://minipro4.pin-dao.cn/static/',
+    dataBaseAddr: 'http://47.113.111.181/static/',
+  };
+  config.io = {
+    namespace: {
+      '/': {
+        connectionMiddleware: [ 'auth' ],
+        packetMiddleware: [ ], // 针对消息的处理暂时不实现
+      },
+    },
   };
   return {
     ...config,
